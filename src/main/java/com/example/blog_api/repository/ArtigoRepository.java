d968129df4c598d4dc307edf179ea4aee5826220
devlucascalado <devlucascalado@gmail.com>
2025-08-13 15:22:47 -0300

    artigo repository

--- src/main/java/com/example/blog_api/repository/ArtigoRepository.java
+++ src/main/java/com/example/blog_api/repository/ArtigoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArtigoRepository extends JpaRepository<Long, Artigo> {
+public interface ArtigoRepository extends JpaRepository<Artigo, Long> {
 
 }
