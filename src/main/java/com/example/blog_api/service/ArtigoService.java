b6d92561293dd46bf9373a69ea0a66d09ce734f9
devlucascalado <devlucascalado@gmail.com>
2025-08-13 15:51:10 -0300

    adicionando metodo atualizar

--- src/main/java/com/example/blog_api/service/ArtigoService.java
+++ src/main/java/com/example/blog_api/service/ArtigoService.java
@@ -4,8 +4,11 @@ package com.example.blog_api.service;
 import com.example.blog_api.model.Artigo;
 import com.example.blog_api.repository.ArtigoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -25,6 +28,19 @@ public class ArtigoService {
 
     public List<Artigo> listarArtigos() {
         return artigoRepository.findAll();
+    }
+
+    public Artigo atualizarArtigo(Long id, Artigo artigo) {
+
+        if (!artigoRepository.existsById(id)) {
+            throw new NoSuchElementException("Artigo com ID " + id + " não encontrado. Não foi possível atualizar.");
+        }
+
+        DateTimeFormatter horaFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        artigo.setDataPublicacao(LocalDateTime.parse(LocalDateTime.now().format(horaFormatada)));
+
+        return artigoRepository.save(artigo);
     }
 
     public void deletarArtigo(Long id) {
