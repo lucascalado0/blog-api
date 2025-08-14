a48e4e14106865ac09c893d74792b3f5f2436afd
devlucascalado <devlucascalado@gmail.com>
2025-08-14 09:23:25 -0300

    artigo service final

--- src/main/java/com/example/blog_api/service/ArtigoService.java
+++ src/main/java/com/example/blog_api/service/ArtigoService.java
@@ -4,11 +4,10 @@ package com.example.blog_api.service;
 import com.example.blog_api.model.Artigo;
 import com.example.blog_api.repository.ArtigoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -19,10 +18,12 @@ public class ArtigoService {
     private ArtigoRepository artigoRepository;
 
     public Artigo novoArtigo(Artigo artigo) {
+        artigo.setDataPublicacao(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+
         return artigoRepository.save(artigo);
     }
 
-    public Optional<Artigo> buscarArtigoPorId(Long id){
+    public Optional<Artigo> buscarArtigoPorId(Long id) {
         return artigoRepository.findById(id);
     }
 
@@ -30,21 +31,24 @@ public class ArtigoService {
         return artigoRepository.findAll();
     }
 
-    public Artigo atualizarArtigo(Long id, Artigo artigo) {
+    public Optional<Artigo> atualizarArtigo(Artigo dadosNovosArtigo, Long id) {
+        Optional<Artigo> artigoExistente = artigoRepository.findById(id);
 
-        if (!artigoRepository.existsById(id)) {
+        if (artigoExistente.isPresent()) {
+            Artigo artigoAtualizado = artigoExistente.get();
+
+            artigoAtualizado.setTitulo(dadosNovosArtigo.getTitulo());
+            artigoAtualizado.setConteudo(dadosNovosArtigo.getConteudo());
+            artigoAtualizado.setDataAtualizacao(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+
+            return Optional.of(artigoRepository.save(artigoAtualizado));
+        } else {
             throw new NoSuchElementException("Artigo com ID " + id + " não encontrado. Não foi possível atualizar.");
         }
-
-        DateTimeFormatter horaFormatada = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-
-        artigo.setDataPublicacao(LocalDateTime.parse(LocalDateTime.now().format(horaFormatada)));
-
-        return artigoRepository.save(artigo);
     }
 
     public void deletarArtigo(Long id) {
-        if (!artigoRepository.existsById(id)){
+        if (!artigoRepository.existsById(id)) {
             throw new NoSuchElementException("Artigo com ID " + id + " não encontrado. Não foi possível deletar.");
         }
         artigoRepository.deleteById(id);
